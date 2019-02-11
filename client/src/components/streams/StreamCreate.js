1657cd91042bf8ccb35dd8b84075f9812f5ab462
Robert Checco <robert.checco@gmail.com>
2019-02-11 14:13:53 -0500

    Add API directory to project for REST API

--- client/src/components/streams/StreamCreate.js
+++ client/src/components/streams/StreamCreate.js
@@ -2,20 +2,41 @@ import React, { Component } from 'react';
 import { Field, reduxForm } from 'redux-form';
 
 class StreamCreate extends Component {
-    renderInput = ({input}) => {
-        return <input {...input}/>
+    renderInput = ({input, label}) => {
+        return (
+            <div className="field">
+                <label>{label}</label>
+                <input {...input}/>
+            </div>
+        );
     }
-
+    onSubmit = (formValues) => {
+        console.log(formValues);
+    }
     render() {
         return (
-            <form>
-                <Field name="title" component={this.renderInput} />
-                <Field name="description" component={this.renderInput} />
+            <form onSubmit={this.props.handleSubmit(this.onSubmit)}className="ui form ">
+                <Field name="title" component={this.renderInput} label='Enter Title'/>
+                <Field name="description" component={this.renderInput} label='Enter Description'/>
+                <button className="ui button primary">Submit</button>
             </form>
         );
     }
 }
 
+const validate = (formValues) => {
+    const errors = {};
+
+    if (!formValues.title) {
+        return errors.title = "Error you did not submit the title.";
+    }
+    if (!formValues.description) {
+        return errors.description = "Error you did not submit the description";
+    }
+    return errors;
+}
+
 export default reduxForm({
-    form: 'streamCreate'
+    form: 'streamCreate',
+    validate
 })(StreamCreate);
